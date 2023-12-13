6d8c4a15e0a5e74520c4c591165fcc6303ac1f81
David <dskpognon20@students.desu.edu>
2023-12-12 20:32:56 -0500

    progress

--- BackingStore.c
+++ BackingStore.c
@@ -1,3 +1,41 @@
 #include "TLB.c"
 #include "PageTable.c"
 #include "PhysicalStorage.c"
+
+#include <stdio.h>
+
+#define PAGE_SIZE 256
+#define NUM_FRAMES 256
+
+// TLB
+int tlb[16][2]; // a 16-entry TLB
+
+// Page table
+int page_table[256];
+
+int main() {
+    // Create a backing store file
+    FILE *backing_store = fopen("BACKING_STORE.bin", "wb");
+    // Write the page data to the backing store
+    // 
+    fclose(backing_store);
+
+    // Load page 251 into Frame 0
+    backing_store = fopen("BACKING_STORE.bin", "rb");
+    fseek(backing_store, 251 * PAGE_SIZE, SEEK_SET);
+    char frame[PAGE_SIZE];
+    fread(frame, sizeof(char), PAGE_SIZE, backing_store);
+    fclose(backing_store);
+
+    // Update the TLB and page table
+    tlb[0][0] = 251; // Page number
+    tlb[0][1] = 0;   // Frame number
+    page_table[251] = 0; // Update the page table with the frame number
+
+    // Fill the frame from 0 to 255
+    for (int i = 0; i < PAGE_SIZE; i++) {
+        printf("%d ", frame[i]); // Output the contents of the frame
+    }
+
+    return 0;
+}
