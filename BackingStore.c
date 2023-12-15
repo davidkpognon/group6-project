685c2c6b3f5dcf43eb065e79b1725a44373a2728
davidkpognon <77710946+davidkpognon@users.noreply.github.com>
2023-12-14 20:57:39 -0500

    Update BackingStore.c

--- BackingStore.c
+++ BackingStore.c
@@ -6,35 +6,59 @@
 
 #define PAGE_SIZE 256
 #define NUM_FRAMES 256
+#define NUM_PAGES 256
+#define TLB_SIZE 16
 
-// TLB
-int tlb[16][2]; // a 16-entry TLB
 
-// Page table
-int page_table[256];
+typedef struct {
+    int page_number;
+    int frame_number;
+} tlb_entry;
+
+
+int page_table[NUM_PAGES];
+// physical memory
+char physical_memory[NUM_FRAMES][PAGE_SIZE];
+
+tlb_entry tlb[TLB_SIZE];
+
+int next_available_frame = 0; 
+
+// function to handle page fault
+void handle_page_fault(int page_number, FILE *backing_store) {
+    // read the page from the backing store 
+    fseek(backing_store, page_number * PAGE_SIZE, SEEK_SET);
+    fread(physical_memory[next_available_frame], sizeof(char), PAGE_SIZE, backing_store);
+    
+    // update 
+    page_table[page_number] = next_available_frame;
+    
+    tlb[next_available_frame % TLB_SIZE].page_number = page_number;
+    tlb[next_available_frame % TLB_SIZE].frame_number = next_available_frame;
+    
+    // increment the next available frame
+    next_available_frame++;
+}
 
 int main() {
-    // Create a backing store file
-    FILE *backing_store = fopen("BACKING_STORE.bin", "wb");
-    // Write the page data to the backing store
-    // 
-    fclose(backing_store);
+    // open the backing store file
+    FILE *backing_store = fopen("BACKING_STORE.bin", "rb");
 
-    // Load page 251 into Frame 0
-    backing_store = fopen("BACKING_STORE.bin", "rb");
-    fseek(backing_store, 251 * PAGE_SIZE, SEEK_SET);
-    char frame[PAGE_SIZE];
-    fread(frame, sizeof(char), PAGE_SIZE, backing_store);
-    fclose(backing_store);
+    if (backing_store == NULL) {
+        fprintf(stderr, "Error: Could not open \n");
+        return 1; 
+    }
 
-    // Update the TLB and page table
-    tlb[0][0] = 251; // Page number
-    tlb[0][1] = 0;   // Frame number
-    page_table[251] = 0; // Update the page table with the frame number
+    // handle page fault for page 251
+    handle_page_fault(251, backing_store);
 
-    // Fill the frame from 0 to 255
-    for (int i = 0; i < PAGE_SIZE; i++) {
-        printf("%d ", frame[i]); // Output the contents of the frame
+    // access the memory located at frame 0 and offset 25 
+    printf("The value at physical address %d\n", physical_memory[0][25]);
+
+
+    if (fclose(backing_store) != 0) {
+        fprintf(stderr, "Error: Could not close\n");
+        return 1;
     }
 
     return 0;
